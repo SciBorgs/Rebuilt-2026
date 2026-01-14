cfb0742257d22c1ce80b0dd7960be6b8355ca61b
Unclesdad <basaltjazz3@gmail.com>
2026-01-14 13:24:15 -0500

    update revlib and add setup tests

--- src/test/java/org/sciborgs1155/robot/RobotTest.java
+++ src/test/java/org/sciborgs1155/robot/RobotTest.java
@@ -1,10 +1,17 @@
 package org.sciborgs1155.robot;
 
 import static org.sciborgs1155.lib.UnitTestingUtil.reset;
+import static org.sciborgs1155.lib.UnitTestingUtil.setupTests;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RobotTest {
+  @BeforeEach
+  public void setup() {
+    setupTests();
+  }
+
   @Test
   void initialize() throws Exception {
     new Robot().close();

4288226f1616156030e4a8bc39c90284c176d5a3
Joseph <jyf2025@gmail.com>
2026-01-14 17:33:53 -0500

    bit of sim

--- src/main/java/org/sciborgs1155/robot/turret/SimTurret.java
+++ src/main/java/org/sciborgs1155/robot/turret/SimTurret.java
@@ -1,10 +1,30 @@
 package org.sciborgs1155.robot.turret;
 
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
+import static edu.wpi.first.units.Units.Radians;
+import static org.sciborgs1155.robot.turret.TurretConstants.*;
+
+import static edu.wpi.first.units.Units.Meters;
+
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 
 public class SimTurret implements TurretIO {
+  private final SingleJointedArmSim sim =
+      new SingleJointedArmSim(
+        DCMotor.getKrakenX60(1),
+        GEAR_RATIO,
+        MOI.in(KilogramSquareMeters),
+        TURRET_LENGTH.in(Meters),
+        MIN_ANGLE.in(Radians),
+        MAX_ANGLE.in(Radians),
+        false, 
+        START_ANGLE.in(Radians)
+      );
+
   @Override
   public void setVoltage(Voltage voltage) {
     // TODO Auto-generated method stub
--- src/main/java/org/sciborgs1155/robot/turret/TurretConstants.java
+++ src/main/java/org/sciborgs1155/robot/turret/TurretConstants.java
@@ -1,12 +1,34 @@
 package org.sciborgs1155.robot.turret;
 
 import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Radian;
+import static edu.wpi.first.units.Units.Radians;
 
 import com.ctre.phoenix6.CANBus;
+
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.MomentOfInertia;
 
 public class TurretConstants {
   public static final CANBus CAN_BUS = new CANBus(); // TODO: Update.
-  public static final double CONVERSION_FACTOR = 1; // TODO: Update.
-  public static final Current CURRENT_LIMIT = Amps.of(67); // TODO: Update.
+  public static final Current CURRENT_LIMIT = Amps.of(60);
+
+  public static final double GEAR_RATIO = 100.0;
+
+  public static final double CONVERSION_FACTOR = (2.0 * Math.PI) / GEAR_RATIO;
+
+  public static final MomentOfInertia MOI = KilogramSquareMeters.of(0.026); // TODO: Update.
+
+  public static final Distance TURRET_LENGTH = Meters.of(0.1);
+
+  public static final Angle MAX_ANGLE = Radians.of(Math.PI);
+  public static final Angle MIN_ANGLE = Radians.of(-Math.PI);
+
+  public static final Angle START_ANGLE = Radians.of(0);
+
 }

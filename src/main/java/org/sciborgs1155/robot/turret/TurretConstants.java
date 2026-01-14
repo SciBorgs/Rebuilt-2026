a4571ad7aebc3c9482f1828413574d5dcf0ad149
TheAk5250 <kumara1@bxscience.edu>
2026-01-14 16:52:05 -0500

    Removed warnings.

--- src/main/java/org/sciborgs1155/robot/turret/TurretConstants.java
+++ src/main/java/org/sciborgs1155/robot/turret/TurretConstants.java
@@ -8,6 +8,5 @@ import edu.wpi.first.units.measure.Current;
 public class TurretConstants {
   public static final CANBus CAN_BUS = new CANBus(); // TODO: Update.
   public static final double CONVERSION_FACTOR = 1; // TODO: Update.
-
   public static final Current CURRENT_LIMIT = Amps.of(67); // TODO: Update.
 }
--- src/main/java/org/sciborgs1155/robot/turret/TurretIO.java
+++ src/main/java/org/sciborgs1155/robot/turret/TurretIO.java
@@ -5,12 +5,7 @@ import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Voltage;
 
 public interface TurretIO {
-  // TODO: Implement.
   public void setVoltage(Voltage voltage);
-
-  // TODO: Implement.
   public Angle getPosition();
-
-  // TODO: Implement.
   public AngularVelocity getVelocity();
 }

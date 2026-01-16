982ee6f57b652a2d433a3b9925182aa49d9df2b8
Dimitri Lesokhin <dimitrilesokhin09@gmail.com>
2026-01-16 16:10:48 -0500

    Finished climb.java - no logger annotations. Added climb visualizer.

--- src/main/java/org/sciborgs1155/robot/climb/Climb.java
+++ src/main/java/org/sciborgs1155/robot/climb/Climb.java
@@ -1,15 +1,12 @@
 package org.sciborgs1155.robot.climb;
 
-
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Volts;
 import static org.sciborgs1155.robot.climb.ClimbConstants.*;
 
-
 import java.util.function.DoubleSupplier;
-
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
@@ -17,21 +14,21 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.networktables.DoubleEntry;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
-
 import org.sciborgs1155.lib.Tuning;
 import org.sciborgs1155.robot.Robot;
 import org.sciborgs1155.robot.climb.ClimbConstants.Level;
-
 
 import com.ctre.phoenix6.SignalLogger;
 public class Climb extends SubsystemBase implements AutoCloseable {
   private final ClimbIO hardware;
   private final SysIdRoutine sysIdRoutine;
+
   private final ProfiledPIDController pid =
       new ProfiledPIDController(
           kP,
@@ -39,26 +36,28 @@ public class Climb extends SubsystemBase implements AutoCloseable {
           kD,
           new TrapezoidProfile.Constraints(
               MAX_VELOCITY.in(MetersPerSecond), MAX_ACCEL.in(MetersPerSecondPerSecond)));
+
   private final ElevatorFeedforward ff = new ElevatorFeedforward(kS, kG, kV);
 
+  private final ElevatorVisualizer setpoint = 
+    new ElevatorVisualizer("setpoint visualizer", new Color8Bit(0, 0, 255));
+
+  private final ElevatorVisualizer measurement = 
+    new ElevatorVisualizer("measurement visualizer", new Color8Bit(255, 0, 0));
 
   private final DoubleEntry S = Tuning.entry("/Robot/tuning/elevator/kS", kS);
   private final DoubleEntry G = Tuning.entry("/Robot/tuning/elevator/kG", kG);
   private final DoubleEntry V = Tuning.entry("/Robot/tuning/elevator/kV", kV);
   private final DoubleEntry A = Tuning.entry("/Robot/tuning/elevator/kA", kA);
 
-
   public Climb(ClimbIO hardware) {
     setDefaultCommand(retract());
 
-
     this.hardware = hardware;
-
 
     pid.setTolerance(POSITION_TOLERANCE.in(Meters));
     pid.reset(hardware.position());
     pid.setGoal(MIN_HEIGHT.in(Meters));
-
 
     sysIdRoutine =
       new SysIdRoutine(
@@ -97,13 +96,11 @@ public class Climb extends SubsystemBase implements AutoCloseable {
     }
   }
 
-
   public boolean atPosition(double position) {
     //given position - |actual position| < tolerance
     return Meters.of(position).minus(Meters.of(position())).magnitude()
         < POSITION_TOLERANCE.in(Meters);
   }
-
 
   public double position() {
     return hardware.position();
@@ -113,17 +110,14 @@ public class Climb extends SubsystemBase implements AutoCloseable {
     return new Climb(Robot.isReal() ? new RealClimb() : new SimClimb());
   }
 
-
   public static Climb none() {
     return new Climb(new NoClimb());
   }
-
 
   @Override
   public void close() throws Exception {
     hardware.close();
   }
-
 
   private void update(double position) {
     double goal =
@@ -137,23 +131,32 @@ public class Climb extends SubsystemBase implements AutoCloseable {
     //Last velocity (current setpoint) and velocity to go to?
     //How does it differentiate?
 
-
     hardware.setVoltage(feedforward + feedback);
     //Did not include the epilogue line
   }
 
+  public double positionSetpoint() {
+    return pid.getSetpoint().position;
+  }
+
+  public void periodic() {
+    setpoint.setLength(positionSetpoint());
+    measurement.setLength(position());
+  }
 
   public Command goTo(DoubleSupplier height) {
     return run(() -> update(height.getAsDouble())).finallyDo(() -> hardware.setVoltage(0));
   }
 
-
   public Command goTo(double height) {
     return goTo(() -> height);
   }
 
-
   public Command retract() {
     return goTo(MIN_HEIGHT.in(Meters)).withName("retracting");
+  }
+
+  public Command extend() {
+    return goTo(MAX_HEIGHT.in(Meters)).withName("extending");
   }
 }
---
+++ src/main/java/org/sciborgs1155/robot/climb/ElevatorVisualizer.java
@@ -0,0 +1,37 @@
+
+package org.sciborgs1155.robot.climb;
+
+import static edu.wpi.first.units.Units.Meters;
+import static org.sciborgs1155.robot.climb.ClimbConstants.MIN_HEIGHT;
+
+import edu.wpi.first.epilogue.NotLogged;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+
+public class ElevatorVisualizer {
+  @NotLogged private final Mechanism2d mech; // The physical field the robot is in
+  private final MechanismLigament2d elevator; // The elevator mechanism
+  private final String name;
+
+  public ElevatorVisualizer(String name, Color8Bit color) {
+    this.name = name;
+    mech = new Mechanism2d(50, 50);
+    MechanismRoot2d chassis = mech.getRoot("chassis", 25, 10);
+    elevator =
+        chassis.append(
+            new MechanismLigament2d("elevator", MIN_HEIGHT.in(Meters) * 10, 90, 3, color));
+  }
+
+  /**
+   * Sets the length of the elevator visualization.
+   *
+   * @param length The length to set.
+   */
+  public void setLength(double length) {
+    elevator.setLength(length * 10);
+    SmartDashboard.putData("Robot/elevator/" + name, mech);
+  }
+}

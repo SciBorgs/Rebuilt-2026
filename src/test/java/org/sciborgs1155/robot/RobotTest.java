75fe2800d13e3d4fa3181d11432e8ef562caeeb0
Joseph <jyf2025@gmail.com>
2026-01-17 11:04:42 -0500

    merged from main

--- src/test/java/org/sciborgs1155/robot/RobotTest.java
+++ src/test/java/org/sciborgs1155/robot/RobotTest.java
@@ -1,7 +1,9 @@
 package org.sciborgs1155.robot;
 
 import static org.sciborgs1155.lib.UnitTestingUtil.reset;
+import static org.sciborgs1155.lib.UnitTestingUtil.setupTests;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RobotTest {

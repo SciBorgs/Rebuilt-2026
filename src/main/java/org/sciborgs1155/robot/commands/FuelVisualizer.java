f5b46bb698e2a5da625ab258f188a7d5e0eed5c7
TheAk5250 <kumara1@bxscience.edu>
2026-01-27 16:19:57 -0500

    Published shooter pose (for viewing in 3D field).

--- src/main/java/org/sciborgs1155/robot/commands/FuelVisualizer.java
+++ src/main/java/org/sciborgs1155/robot/commands/FuelVisualizer.java
@@ -14,6 +14,7 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructArrayPublisher;
+import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -76,6 +77,12 @@ public final class FuelVisualizer {
           .getStructArrayTopic("Fuel Visualizer/Fuel Poses", Pose3d.struct)
           .publish();
 
+  /** A publisher for the orientation of the shooter. */
+  private static StructPublisher<Pose3d> shooterPose =
+      NetworkTableInstance.getDefault()
+          .getStructTopic("Fuel Visualizer/Shooter Pose", Pose3d.struct)
+          .publish();
+
   /**
    * To be called on robot startup. Parameters used to calculate Fuel trajectory after launch.
    *
@@ -123,6 +130,7 @@ public final class FuelVisualizer {
 
     // PUBLISHING DATA
     fuelPosePublisher.accept(fuelPoses);
+    shooterPose.accept(shooterPose());
   }
 
   /**

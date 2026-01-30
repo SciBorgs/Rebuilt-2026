cb79812a3388eecb53337e243833e0821cfd5829
Stephanie2028 <Stephaniezhu07@gmail.com>
2026-01-30 17:11:35 -0500

    bb

--- src/main/java/org/sciborgs1155/robot/indexer/Indexer.java
+++ src/main/java/org/sciborgs1155/robot/indexer/Indexer.java
@@ -9,24 +9,32 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import org.sciborgs1155.lib.Beambreak;
 import org.sciborgs1155.lib.SimpleMotor;
 import org.sciborgs1155.robot.Robot;
 
 public class Indexer extends SubsystemBase implements AutoCloseable {
   private final SimpleMotor hardware;
+  private final Beambreak beambreak; 
+  public final Trigger blocked; 
 
-  private Indexer(SimpleMotor hardware) {
+  private Indexer(SimpleMotor hardware, Beambreak beambreak) {
     this.hardware = hardware;
+    this.beambreak = beambreak;
 
-    setDefaultCommand(null);
+    this.blocked = new Trigger(() -> !beambreak.getState());
+
+    setDefaultCommand(stop());
   }
 
   public static Indexer create() {
-    return (Robot.isReal()) ? new Indexer(realMotor()) : none();
+    return (Robot.isReal()) ? new Indexer(realMotor(), Beambreak.real(BEAMBREAK)) : none();
   }
 
   public static Indexer none() {
-    return new Indexer(SimpleMotor.none());
+    return new Indexer(SimpleMotor.none(), Beambreak.none());
   }
 
   public static SimpleMotor realMotor() {

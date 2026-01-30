24730ed093f5a7a9b84db0708031dd3e2bb48f8c
Stephanie2028 <Stephaniezhu07@gmail.com>
2026-01-30 16:59:00 -0500

    wow

--- src/main/java/org/sciborgs1155/robot/indexer/Indexer.java
+++ src/main/java/org/sciborgs1155/robot/indexer/Indexer.java
@@ -1,66 +1,62 @@
 package org.sciborgs1155.robot.indexer;
 
-import static org.sciborgs1155.robot.indexer.IndexerConstants.*;
 import static edu.wpi.first.units.Units.Amps;
 import static org.sciborgs1155.robot.Ports.Indexer.*;
-import org.sciborgs1155.lib.SimpleMotor;
-import org.sciborgs1155.robot.Robot;
+import static org.sciborgs1155.robot.indexer.IndexerConstants.*;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.sciborgs1155.lib.SimpleMotor;
+import org.sciborgs1155.robot.Robot;
 
 public class Indexer extends SubsystemBase implements AutoCloseable {
-    private final SimpleMotor hardware;
+  private final SimpleMotor hardware;
 
-    private Indexer(SimpleMotor hardware){
-        this.hardware = hardware; 
+  private Indexer(SimpleMotor hardware) {
+    this.hardware = hardware;
 
-        setDefaultCommand(null);
-    }
+    setDefaultCommand(null);
+  }
 
-    public static Indexer create(){
-        return (Robot.isReal())? new Indexer(realMotor()): none();
-    }
+  public static Indexer create() {
+    return (Robot.isReal()) ? new Indexer(realMotor()) : none();
+  }
 
-    public static Indexer none(){
-        return new Indexer(SimpleMotor.none());
-    }
+  public static Indexer none() {
+    return new Indexer(SimpleMotor.none());
+  }
 
-    public static SimpleMotor realMotor(){
-        TalonFX motor = new TalonFX(MOTOR); 
-        TalonFXConfiguration config = new TalonFXConfiguration();
+  public static SimpleMotor realMotor() {
+    TalonFX motor = new TalonFX(MOTOR);
+    TalonFXConfiguration config = new TalonFXConfiguration();
 
-        config.CurrentLimits.SupplyCurrentLimit = CURRENT_LIMIT.in(Amps);
-        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    config.CurrentLimits.SupplyCurrentLimit = CURRENT_LIMIT.in(Amps);
+    config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
-        return SimpleMotor.talon(motor, config);
-    }
+    return SimpleMotor.talon(motor, config);
+  }
 
-    public Command runIntake(double power){
-        return run(() -> hardware.set(power));
-    }
+  public Command runIntake(double power) {
+    return run(() -> hardware.set(power));
+  }
 
-    public Command forward(){
-        return runIntake(INTAKE_POWER);
-    }
+  public Command forward() {
+    return runIntake(INTAKE_POWER);
+  }
 
-    public Command backward(){
-        return runIntake(-INTAKE_POWER);
-    }
+  public Command backward() {
+    return runIntake(-INTAKE_POWER);
+  }
 
-    public Command stop(){
-        return runIntake(0);
-    }
+  public Command stop() {
+    return runIntake(0);
+  }
 
-    @Override
-    public void close() throws Exception{
-        hardware.close(); 
-    }
-
-
-    
+  @Override
+  public void close() throws Exception {
+    hardware.close();
+  }
 }
--- src/main/java/org/sciborgs1155/robot/indexer/IndexerConstants.java
+++ src/main/java/org/sciborgs1155/robot/indexer/IndexerConstants.java
@@ -1,9 +1,10 @@
 package org.sciborgs1155.robot.indexer;
 
+import static edu.wpi.first.units.Units.Amps;
+
 import edu.wpi.first.units.measure.Current;
 
 public class IndexerConstants {
-    public static final Current CURRENT_LIMIT = ; 
-    public static final int INTAKE_POWER = 0;
-    
+  public static final Current CURRENT_LIMIT = Amps.of(0);
+  public static final int INTAKE_POWER = 0;
 }

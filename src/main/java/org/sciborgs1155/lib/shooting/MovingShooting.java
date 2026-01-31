e53ae2bdff61ace4a3b9fe6e2335b5790796dfe1
TheAk5250 <kumara1@bxscience.edu>
2026-01-31 16:28:55 -0500

    IT'S ALIVE!

--- src/main/java/org/sciborgs1155/lib/shooting/MovingShooting.java
+++ src/main/java/org/sciborgs1155/lib/shooting/MovingShooting.java
@@ -2,13 +2,10 @@ package org.sciborgs1155.lib.shooting;
 
 import static org.sciborgs1155.robot.FieldConstants.HUB;
 
-import org.sciborgs1155.robot.FieldConstants;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.numbers.N3;
@@ -19,36 +16,28 @@ public class MovingShooting implements ShootingAlgorithm {
 
   // constants
   double latency = 0.1;
-  double totalSpeed = 10;
+  double exitSpeed = 10;
 
   double magnusCorrection = 1;
 
   @Override
   public Vector<N3> calculate(Translation2d pose, Vector<N2> velocity) {
-    // robot pos, robot velocity, and project pos, goal po
-    Vector<N2> robotPos = pose.toVector();
-    Vector<N2> projectedPos = robotPos.plus(velocity).times(latency);
-    Vector<N2> goalPos = HUB.toTranslation2d().toVector();
-
     // target vector (from robot to goal not moving)
-    Vector<N2> target = goalPos.minus(projectedPos);
-
-    // shot vector (from robot to goal with moving)
-    Vector<N2> shot = target.minus(velocity);
+    Vector<N2> target = HUB.toTranslation2d().toVector().minus(pose.toVector());
 
     // angle to shoot at goal
-    double angle = Math.atan2(shot.get(1), shot.get(0));
+    double angle = Math.atan2(target.get(1), target.get(0));
     
     // shot speed
-    double speed = shot.norm();
+    double requiredSpeed = target.norm();
 
     // pitch to shoot at goal
-    double pitch = Math.acos(MathUtil.clamp(speed / totalSpeed, 0 ,1));
+    double pitch = Math.acos(MathUtil.clamp(requiredSpeed / exitSpeed, 0 ,1));
 
     // final X, Y, and Z
-    double finalX = Math.cos(angle) * totalSpeed;
-    double finalY = Math.sin(angle) * totalSpeed;
-    double finalZ = Math.sin(pitch) * totalSpeed;
+    double finalX = Math.cos(angle) * exitSpeed;
+    double finalY = Math.sin(angle) * exitSpeed;
+    double finalZ = Math.sin(pitch) * exitSpeed;
 
     return VecBuilder.fill(finalX,finalY,finalZ);
   }

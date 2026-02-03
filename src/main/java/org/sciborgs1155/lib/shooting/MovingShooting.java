e330e4b80ead44939c9e447a01ba1754b61404fc
StarlitHaruka <harukasmain@gmail.com>
2026-02-03 17:49:19 -0500

    more testing - temp changes

--- src/main/java/org/sciborgs1155/lib/shooting/MovingShooting.java
+++ src/main/java/org/sciborgs1155/lib/shooting/MovingShooting.java
@@ -2,9 +2,6 @@ package org.sciborgs1155.lib.shooting;
 
 import static org.sciborgs1155.robot.FieldConstants.HUB;
 
-import org.sciborgs1155.robot.Constants.Robot;
-
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -12,6 +9,7 @@ import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.numbers.N3;
 
 public class MovingShooting implements ShootingAlgorithm {
+
   // constants
   double launchSpeed = 6.7;
 
@@ -19,12 +17,18 @@ public class MovingShooting implements ShootingAlgorithm {
   public Vector<N3> calculate(Translation2d pose, Vector<N2> velocity) {
     // target vector (from robot to goal not moving)
     Vector<N2> target = HUB.toTranslation2d().toVector().minus(pose.toVector());
+    
+    Vector<N2> shot = target.minus(velocity);
 
     // angle to shoot at goal
-    double angle = Math.atan2(target.get(1), target.get(0));
+    double angle = Math.atan2(shot.get(1), shot.get(0));
+    
+    // shot speed
+    double requiredSpeed = shot.norm();
 
     // pitch to shoot at goal
-    double pitch = Math.acos(target.norm() * Math.sqrt(9.81 / (2 * (HUB.getZ()))) / launchSpeed);
+    if (launchSpeed < requiredSpeed) return VecBuilder.fill(0,0,0);
+    double pitch = Math.acos(shot.norm() * Math.sqrt(9.81 / (2 * (HUB.getZ()))) / launchSpeed);
 
     // final X, Y, and Z
     double finalX = Math.cos(angle) * launchSpeed;

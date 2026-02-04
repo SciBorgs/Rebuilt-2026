b6ed4b0f1a41bbf46c5962553332e29f4d82632a
unknown <sciborgs@bxscience.edu>
2026-02-04 17:57:37 -0500

    the issue was gyro displacement with vision poses. changed chassis constants and made rotation stddevs super high

--- src/main/java/org/sciborgs1155/robot/vision/VisionConstants.java
+++ src/main/java/org/sciborgs1155/robot/vision/VisionConstants.java
@@ -104,8 +104,8 @@ public class VisionConstants {
   public static final int WIDTH = 1280;
   public static final int HEIGHT = 720;
 
-  public static final Matrix<N3, N1> SINGLE_TAG_STD_DEVS = VecBuilder.fill(0.6, 0.6, 1.3);
-  public static final Matrix<N3, N1> MULTIPLE_TAG_STD_DEVS = VecBuilder.fill(0.3, 0.3, 1.1);
+  public static final Matrix<N3, N1> SINGLE_TAG_STD_DEVS = VecBuilder.fill(0.6, 0.6, 1155); //TODO decide these later when we test the bump
+  public static final Matrix<N3, N1> MULTIPLE_TAG_STD_DEVS = VecBuilder.fill(0.3, 0.3, 1155);
   public static final Matrix<N3, N1> SUPERTRUST_TAG_STD_DEVS = VecBuilder.fill(0.001, 0.001, 0.001);
 
   public static final double MAX_HEIGHT = 0.305;

b9c2ad4ec8e5d3c9cfea3baa6bfcbcb6a24de3bf
Dimitri Lesokhin <dimitrilesokhin09@gmail.com>
2026-02-06 16:27:59 -0500

    Changed from spring loaded climb sim to Elevator sim without gravity because the spring opposes gravity but the brake doesn't let the spring push the climb up.

--- src/main/java/org/sciborgs1155/robot/climb/SimClimb.java
+++ src/main/java/org/sciborgs1155/robot/climb/SimClimb.java
@@ -2,24 +2,28 @@ package org.sciborgs1155.robot.climb;
 
 import static edu.wpi.first.units.Units.Kilograms;
 import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Seconds;
-import static org.sciborgs1155.robot.Constants.*;
-import static org.sciborgs1155.robot.climb.ClimbConstants.*;
+import static org.sciborgs1155.robot.Constants.PERIOD;
+import static org.sciborgs1155.robot.climb.ClimbConstants.GEARING;
+import static org.sciborgs1155.robot.climb.ClimbConstants.MAX_HEIGHT;
+import static org.sciborgs1155.robot.climb.ClimbConstants.MIN_HEIGHT;
+import static org.sciborgs1155.robot.climb.ClimbConstants.SPROCKET_RADIUS;
+import static org.sciborgs1155.robot.climb.ClimbConstants.STARTING_HEIGHT;
+import static org.sciborgs1155.robot.climb.ClimbConstants.WEIGHT;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import org.sciborgs1155.lib.SpringLoadedClimbSim;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 
 public class SimClimb implements ClimbIO {
-  private final SpringLoadedClimbSim climb =
-      new SpringLoadedClimbSim(
+  private final ElevatorSim climb =
+      new ElevatorSim(
           LinearSystemId.createElevatorSystem(
               DCMotor.getKrakenX60(2), WEIGHT.in(Kilograms), SPROCKET_RADIUS.in(Meters), GEARING),
           DCMotor.getKrakenX60(2),
           MIN_HEIGHT.in(Meters),
           MAX_HEIGHT.in(Meters),
-          SPRING_ACCELERATION.in(MetersPerSecondPerSecond),
+          false,
           STARTING_HEIGHT.in(Meters));
 
   /** Constructor of the climb simulator */

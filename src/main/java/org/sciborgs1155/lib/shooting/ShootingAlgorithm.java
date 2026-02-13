65c5c6ba4ed1dcd96e36f01212cd759072927e1b
Masua1 <masuac@nycstudents.net>
2026-02-13 15:16:20 -0500

    work

--- src/main/java/org/sciborgs1155/lib/shooting/ShootingAlgorithm.java
+++ src/main/java/org/sciborgs1155/lib/shooting/ShootingAlgorithm.java
@@ -2,7 +2,6 @@ package org.sciborgs1155.lib.shooting;
 
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Radians;
-import static edu.wpi.first.units.Units.RadiansPerSecond;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation3d;
@@ -37,31 +36,30 @@ public interface ShootingAlgorithm {
     InterpolatingDoubleTreeMap velocityLookup = new InterpolatingDoubleTreeMap();
 
     // have magnus force + drag incorporated
-    velocityLookup.put(2, 781.2063637 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(2.5, 651.4441804 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(3, 628.0489819 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(3.5, 630.7480891 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(4, 643.4548917 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(4.5, 660.6486686 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(5, 680.1591875 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(5.5, 700.8373228 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(6, 722.0939247 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
+    velocityLookup.put(2, 781.2063637 - drive.robotRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(2.5, 651.4441804 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(3, 628.0489819 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(3.5, 630.7480891 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(4, 643.4548917 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(4.5, 660.6486686 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(5, 680.1591875 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(5.5, 700.8373228 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(6, 722.0939247 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
     velocityLookup.put(
-        6.121768808, 727.3166827 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
+        6.121768808, 727.3166827 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(7.2898, 777.6028308 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(7.5, 786.617125 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(8, 807.9529887 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(8.5, 829.1007001 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(9, 850.0372997 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(9.5, 870.7462475 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(10, 891.2216084 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(10.5, 911.4618775 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(11, 931.4696372 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(11.5, 951.2425991 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
+    velocityLookup.put(12, 970.7926058 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
     velocityLookup.put(
-        7.2898, 777.6028308 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(7.5, 786.617125 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(8, 807.9529887 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(8.5, 829.1007001 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(9, 850.0372997 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(9.5, 870.7462475 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(10, 891.2216084 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(10.5, 911.4618775 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(11, 931.4696372 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(11.5, 951.2425991 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(12, 970.7926058 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
-    velocityLookup.put(
-        12.5831802, 991.3452656 - drive.fieldRelativeChassisSpeeds().in(RadiansPerSecond));
+        12.5831802, 991.3452656 - drive.fieldRelativeChassisSpeeds().vxMetersPerSecond);
   }
 
   public static double calculatePosition(Pose2d position) {

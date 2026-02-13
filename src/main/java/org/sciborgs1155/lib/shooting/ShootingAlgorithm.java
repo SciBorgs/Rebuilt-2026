725bd70fdcf620f6f8d146983ac3c0d1865c145f
Masua1 <masuac@nycstudents.net>
2026-02-12 21:21:39 -0500

    made lookup table

--- src/main/java/org/sciborgs1155/lib/shooting/ShootingAlgorithm.java
+++ src/main/java/org/sciborgs1155/lib/shooting/ShootingAlgorithm.java
@@ -1,9 +1,19 @@
 package org.sciborgs1155.lib.shooting;
 
-import edu.wpi.first.math.Vector;
+import edu.wpi.first.units.Units.Meters;
+import edu.wpi.first.units.Units.MetersPerSecond;
+import edu.wpi.first.units.Units.Radians;
+import org.sciborgs1155.robot.drive.*;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.numbers.N2;
-import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.units.Units.MetersPerSecond;
+import edu.wpi.first.units.Units.Radians;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.LinearVelocity;
 
 @FunctionalInterface
 public interface ShootingAlgorithm {
@@ -17,5 +27,59 @@ public interface ShootingAlgorithm {
    * @param velocity The current translational velocity of the shooter.
    * @return The direction and speed to run the shooter to shoot accurately towards the goal.
    */
-  Vector<N3> calculate(Translation3d pose, Vector<N2> velocity);
+  ShootingAlgorithm calculate(Translation3d pose, ChassisSpeeds velocity);
+
+  double translationX = 5;
+  double translationY = 20;
+  double hubX = 4.0345;
+  double hubY = 3.057;
+  double hubZ = 1.827276;
+  double shooterZ = 0.254;
+
+  InterpolatingDoubleTreeMap velocityLookup = new InterpolatingDoubleTreeMap();
+  
+  //have magnus force + drag incorporated
+  velocityLookup.put(2,781.2063637 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(2.5,651.4441804 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(3,628.0489819 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(3.5,630.7480891 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(4,643.4548917 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(4.5,660.6486686 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(5,680.1591875 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(5.5,700.8373228 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(6,722.0939247 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(6.121768808,727.3166827 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(7.2898,777.6028308 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(7.5,786.617125 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(8,807.9529887 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(8.5,829.1007001 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(9,850.0372997 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(9.5,870.7462475 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(10,891.2216084 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(10.5,911.4618775 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(11,931.4696372 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(11.5,951.2425991 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(12,970.7926058 - drive.fieldRelativeChassisSpeeds());
+  velocityLookup.put(12.5831802,991.3452656 - drive.fieldRelativeChassisSpeeds());
+  
+  
+  
+  public static double calculatePosition(Pose2d position) {    
+    
+    Translation3d pose =
+        new Translation3d(
+            position.getX() + translationX, position.getX() + translationY, shooterZ);
+    Translation3d hub = new Translation3d(hubX, hubY, hubZ);
+
+    double hDistance = Math.sqrt(Math.pow(hubX - pose.getX(), 2) + Math.pow(hubY- pose.getY(), 2));
+    double vDistance = hubZ - shooterZ;
+
+    LinearVelocity fuelVelocity = MetersPerSecond.of(5);
+
+    Angle currentXangle =
+        Radians.of(90 / (Math.sqrt(Math.pow(pose.getX(), 2) + Math.pow(pose.getY(), 2))));
+    Angle neededXAngle =
+        Radians.of(90 / (Math.sqrt(Math.pow(hub.getX(), 2) + Math.pow(hub.getY(), 2))));
+    return neededXAngle.minus(currentXangle).in(Radians);
+        }
 }

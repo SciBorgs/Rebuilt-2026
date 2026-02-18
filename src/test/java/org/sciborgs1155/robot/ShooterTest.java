5d2210158d24b2dfeb670f4554aaf3f561bd2cd1
Unclesdad <basaltjazz3@gmail.com>
2026-02-17 20:56:46 -0500

    fixed randVelocity in ShooterTest to not use goToTest

--- src/test/java/org/sciborgs1155/robot/ShooterTest.java
+++ src/test/java/org/sciborgs1155/robot/ShooterTest.java
@@ -4,12 +4,16 @@ import static edu.wpi.first.units.Units.*;
 import static org.sciborgs1155.lib.Test.runUnitTest;
 import static org.sciborgs1155.lib.UnitTestingUtil.reset;
 import static org.sciborgs1155.lib.UnitTestingUtil.setupTests;
+import static org.sciborgs1155.lib.UnitTestingUtil.fastForward;
 import static org.sciborgs1155.robot.shooter.ShooterConstants.*;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 import org.sciborgs1155.robot.shooter.Shooter;
+
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public class ShooterTest {
   private Shooter shooter;
@@ -39,6 +43,10 @@ public class ShooterTest {
   @RepeatedTest(5)
   public void randVelocity() throws Exception {
     double val = Math.random();
-    runUnitTest(shooter.goToTest(() -> val * MAX_VELOCITY.in(RadiansPerSecond)));
+    double setpoint = val * MAX_VELOCITY.in(RadiansPerSecond);
+    CommandScheduler.getInstance()
+        .schedule(shooter.runShooter(setpoint).withDeadline(Commands.waitSeconds(3)));
+    fastForward(Seconds.of(3));
+    assert shooter.atVelocity(setpoint);
   }
 }

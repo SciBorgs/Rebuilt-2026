6200a808ebed68629ae7666bac59231270479b8e
StarlitHaruka <harukasmain@gmail.com>
2026-02-28 16:19:25 -0500

    am cry

--- src/main/java/org/sciborgs1155/robot/commands/shooting/MovingShooting.java
+++ src/main/java/org/sciborgs1155/robot/commands/shooting/MovingShooting.java
@@ -1,14 +1,19 @@
 package org.sciborgs1155.robot.commands.shooting;
 
+import static java.lang.Math.atan2;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 import static org.sciborgs1155.robot.commands.shooting.ProjectileVisualizer.Projectile.X;
 import static org.sciborgs1155.robot.commands.shooting.ProjectileVisualizer.Projectile.Y;
 import static org.sciborgs1155.robot.commands.shooting.ShotOptimizer.*;
+
+import org.sciborgs1155.lib.LoggingUtils;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 public class MovingShooting {
-  public static double[] calculateLaunchParameters(Pose3d robotPose, ChassisSpeeds velocity) {
+  public static double[] calculateLaunchParameters(Pose3d robotPose, ChassisSpeeds robotVelocity) {
     double heading = robotPose.getRotation().getZ();
     double[] robotToShooter = FuelVisualizer.robotToShooter(robotPose);
     double[] shooterPose = {
@@ -19,11 +24,19 @@ public class MovingShooting {
     double xDisplacement = GOAL[X] - shooterPose[X];
 
     double yaw = Math.atan2(yDisplacement, xDisplacement);
+ 
     double distance = Math.hypot(yDisplacement, xDisplacement);
 
     double speed = ShotTable.speed(distance);
     double pitch = ShotTable.angle(distance);
 
-    return new double[] {distance, speed, pitch, yaw - heading};
+    double[] shotVelocity = {speed * cos(yaw - heading), speed * sin(yaw - heading), speed * sin(pitch)};
+    double[] shooterVelocity = FuelVisualizer.shooterVelocity(shotVelocity, robotPose, robotVelocity);
+    
+    double finalYaw = atan2(shotVelocity[Y] - shooterVelocity[Y], shotVelocity[X] - shooterVelocity[X]);
+    LoggingUtils.log("yaw", finalYaw);
+
+    return new double[] {distance, speed, pitch, finalYaw};
+
   }
 }

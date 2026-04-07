8f26973fddafab313b927d871edb781c476f1e72
Dimitri Lesokhin <dimitrilesokhin09@gmail.com>
2026-04-07 12:26:12 -0400

    added PID to hopper

--- src/main/java/org/sciborgs1155/robot/hopper/HopperConstants.java
+++ src/main/java/org/sciborgs1155/robot/hopper/HopperConstants.java
@@ -1,12 +1,21 @@
 package org.sciborgs1155.robot.hopper;
 
 import static edu.wpi.first.units.Units.Amps;
-
 import edu.wpi.first.units.measure.Current;
 
 public class HopperConstants {
 
   public static final Current CURRENT_LIMIT = Amps.of(30);
-  public static final double INTAKING_POWER = 1;
-  public static final double INTAKING_VOLTAGE = 4;
+  public static final double INTAKING_VELOCITY = 1;
+
+  public static final double P = 0.0;
+  public static final double I = 0.0;
+  public static final double D = 0.0;
+
+  public static final double S = 0.0;
+  public static final double V = 0.0;
+  public static final double A = 0.0;
+
+  public static final double MAX_VOLTAGE = 9;
+
 }

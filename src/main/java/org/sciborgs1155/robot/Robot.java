d98c1820c025c20ebb0fa374b1d99aa22050c47e
unknown <sciborgs@bxscience.edu>
2026-04-08 14:33:42 -0400

    failsafe

--- src/main/java/org/sciborgs1155/robot/Robot.java
+++ src/main/java/org/sciborgs1155/robot/Robot.java
@@ -75,13 +75,13 @@ public class Robot extends CommandRobot {
   // SUBSYSTEMS
   private final Drive drive = Drive.create();
   private final Vision vision = Vision.create();
-  private final Intake intake = Intake.create();
+  private final Intake intake = Intake.none(); // no opening we defense
   private final Turret turret = Turret.create();
   private final Hood hood = Hood.create();
   private final Shooter shooter = Shooter.create();
   private final Indexer indexer = Indexer.create();
   private final Hopper hopper = Hopper.create();
-  private final Slapdown slapdown = Slapdown.create();
+  private final Slapdown slapdown = Slapdown.none(); // no opening we defense
   private final Climb climb = Climb.none();
   private final LEDs leds = LEDs.create();
 
